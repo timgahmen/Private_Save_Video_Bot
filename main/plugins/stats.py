a1051a34f8506394ecf5b468ccef1920b8135b63
sumit10869 <142525697+sumit10869@users.noreply.github.com>
2024-06-10 20:58:49 +0530

    Update stats.py

--- main/plugins/stats.py
+++ main/plugins/stats.py
@@ -56,7 +56,7 @@ async def stats(event):
             f'Memory Total: {mem_t}\n'\
             f'Memory Free: {mem_a}\n'\
             f'Memory Used: {mem_u}\n'\
-            f'Powered by **__[Team SPY](https://t.me/dev_gagan)__**\n'
+            f'Powered by **__[๛𝐌𝐑๛𝐒𝐀𝐓𝐘𝐀𝐌๛](tg://openmessage?user_id=6090912349)__**\n'
     
     await event.reply(f"{stats}")
    #duz.edit(stats)
